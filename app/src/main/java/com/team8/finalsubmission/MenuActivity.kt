b947cf876a73d137c1d4963c8849c8f50b37ddff
kafuowi <pressy286@gmail.com>
2022-12-15 15:12:55 +0900

    토스트 메세지 제거

--- app/src/main/java/com/team8/finalsubmission/MenuActivity.kt
+++ app/src/main/java/com/team8/finalsubmission/MenuActivity.kt
@@ -156,8 +156,7 @@ class MenuActivity : AppCompatActivity(){
 
                 categoryAdapter.setOnItemClickListener(object : MenuListAdapterCategory.OnItemClickListener {
                     override fun onItemClick(v: View, data: CategoryData, pos: Int) {//카테고리 선택 click listener
-                        //Toast.makeText(v.context, "${data.name} Click!", Toast.LENGTH_SHORT)
-                            .show()
+                        //Toast.makeText(v.context, "${data.name} Click!", Toast.LENGTH_SHORT) .show()
 
                         databaseMenu	=	Firebase.database.getReference("menus/${data.name}")//클릭한 카테고리 데이터베이스 가져오기
                         refreshGridData(listManager,listAdapter)//메뉴 그리드 갱신
